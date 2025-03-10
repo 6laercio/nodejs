a13bf6f1230eb11a22157a9c1d90ee2fdcbdf147
Laercio Nascimento <laercio@protonmail.ch>
2025-03-10 01:31:22 -0300

    Add routes

--- http/src/server.js
+++ http/src/server.js
@@ -1,9 +1,15 @@
 import http from "node:http";
 
 const server = http.createServer((req, res) => {
-  const body = "hello world";
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end(body);
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/tasks")
+    return res.end("Route to get tasks");
+
+  if (method === "POST" && url === "/tasks")
+    return res.end("Route to create task");
+
+  return res.end("Not found");
 });
 
 server.listen(3000, () => {
