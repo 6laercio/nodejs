b2cda2491aab5c9ac906586562bd1d9d9f92fbe3
Laercio Nascimento <laercio@protonmail.ch>
2025-03-10 17:47:36 -0300

    Add writable stream and new readable stream example

--- streams/src/server.js
+++ streams/src/server.js
@@ -1,4 +1,4 @@
-import { Readable } from "node:stream";
+import { Readable, Writable } from "node:stream";
 
 class CounterStream extends Readable {
   count = 1;
@@ -9,7 +9,7 @@ class CounterStream extends Readable {
       if (this.count > this.max) {
         this.push(null);
       } else {
-        this.push(`Número: ${this.count}\n`);
+        this.push(String(this.count));
         this.count += 1;
       }
       clearTimeout(timer);
@@ -17,5 +17,51 @@ class CounterStream extends Readable {
   }
 }
 
+class EvenOddCheckerStream extends Writable {
+  _write(chunk, encoding, callback) {
+    const num = Number(chunk.toString());
+    const result = num % 2 === 0 ? "par" : "ímpar";
+    console.log(`${num} é ${result}`);
+    callback();
+  }
+}
+
 const counterStream = new CounterStream();
-counterStream.pipe(process.stdout);
+const evenOddChecker = new EvenOddCheckerStream();
+
+counterStream.pipe(evenOddChecker);
+
+/* function createArrayStream() {
+  const data = [
+    "primeira linha",
+    "segunda linha",
+    "terceira linha",
+    "quarta linha",
+    "quinta linha",
+  ];
+
+  const readableStream = new Readable({
+    objectMode: true,
+    read() {
+      if (data.length === 0) {
+        this.push(null); // Indica o fim da stream
+      } else {
+        const chunk = data.shift();
+        this.push(chunk);
+      }
+    },
+  });
+
+  // Consumindo a stream
+  readableStream.on("data", (chunk) => {
+    console.log(`Recebido: ${chunk}`);
+  });
+
+  readableStream.on("end", () => {
+    console.log("Stream finalizada");
+  });
+
+  return readableStream;
+}
+
+createArrayStream(); */
