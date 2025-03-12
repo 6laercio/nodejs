f3b6e94de0e77db5c6f74b0d508c7e8adba77583
Laercio Nascimento <laercio@protonmail.ch>
2025-03-12 02:05:21 -0300

    Implementa persistência de dados em arquivo JSON

--- http/src/database.js
+++ http/src/database.js
@@ -1,17 +1,39 @@
+import fs from "node:fs/promises";
+
+const dbPath = new URL("../db.json", import.meta.url);
+
 class Database {
   #database = {};
 
+  constructor() {
+    this.#load();
+  }
+
+  async #load() {
+    try {
+      const data = await fs.readFile(dbPath, "utf-8");
+      this.#database = JSON.parse(data);
+    } catch (error) {
+      this.#persist();
+    }
+  }
+
+  async #persist() {
+    await fs.writeFile(dbPath, JSON.stringify(this.#database));
+  }
+
   select(table) {
-    const data = this.#database[table] ?? [];
-    return data;
+    return this.#database[table] ?? [];
   }
 
   insert(table, data) {
-    if (Array.isArray(this.#database[table])) {
-      this.#database[table].push(data);
-    } else {
-      this.#database[table] = [data];
+    if (!this.#database[table]) {
+      this.#database[table] = [];
     }
+
+    this.#database[table].push(data);
+    this.#persist();
+
     return data;
   }
 
